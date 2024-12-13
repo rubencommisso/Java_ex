6c7cf6c2afa300165411f6082f50f807c8fd3cb5
rubencommisso <commissoruben@gmail.com>
2024-12-13 11:21:52 +0100

    add if/else

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-let banana = 10;
+/* let banana = 10;
 const pera = 20;
 
 console.log(banana)
-console.log(pera)
+console.log(pera) */
 
-let number = 5
+/* let number = 5
 {let number = 6; 
     console.log(number)
 } console.log(number)
@@ -25,7 +25,7 @@ console.log(giacomo)
 } console.log(outside)
     console.log(inside) */
 
-for (let i = 0; i <= 20; i++) {
+/* for (let i = 0; i <= 20; i++) {
         if (i % 2 === 0) {
             console.log(i);
         }
@@ -38,15 +38,23 @@ let numero = 10;
             console.log(numero);
         }
         numero--;
-    }
+    } */
 
 /* let word = prompt("Inserisci una parola");
 console.log(word.length); */
 
-let word;
+/* let word;
 
 do {
     word = prompt("Inserisci una parola di almeno 5 caratteri:");
 } while (word.length < 5);
 
-console.log("Hai inserito la parola: " + word);
+console.log("Hai inserito la parola: " + word); */
+
+let number = 10
+
+if (number > 5) {
+    console.log ("La variabile è maggiore di 5");
+} else {
+    console.log ("la variabile è minore di 5");
+}
