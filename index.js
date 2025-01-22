f37bac282e896e20249ab5a27658dc60cb837e29
rubencommisso <commissoruben@gmail.com>
2025-01-22 12:06:00 +0100

    add save and delete a coockie

--- index.js
+++ index.js
@@ -1755,7 +1755,7 @@ f3(); */
          */
 
 
-const promise1 = new Promise ((resolve, reject) => {
+/* const promise1 = new Promise ((resolve, reject) => {
     setTimeout (() => {
         const success = true;
         if(success) {
@@ -1784,11 +1784,49 @@ Promise.all([promise1,promise2])
     })
     .catch((error) => {
         console.error("Errore", error);
-    });
+    }); */
 
+    
+    function setCookie(cname, cvalue, exdays) {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        let expires = "expires="+d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+      }
+      
+      function getCookie(cname) {
+        let name = cname + "=";
+        let ca = document.cookie.split(';');
+        for(let i = 0; i < ca.length; i++) {
+          let c = ca[i];
+          while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+          }
+          if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+          }
+        }
+        return "";
+      }
+      
+      function checkCookie() {
+        let user = getCookie("username");
+        if (user != "") {
+          alert("Welcome again " + user);
+        } else {
+          user = prompt("Please enter your name:", "");
+          if (user != "" && user != null) {
+            setCookie("username", user, 365);
+          }
+        }
+      } 
 
+      function deleteCookie(cname) {
+        document.cookie = `${cname}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+      }
 
+      setCookie ("username", "Ruben", 1)
 
-
-
-                
+      console.log(getCookie("username"))
+        
+      deleteCookie("username")
